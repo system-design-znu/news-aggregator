155f8330afc76ac338c088808dc8af87a6047562
0-Behnam-0 <Mahdi.mohamadiha.znu@gmail.com>
2022-12-18 16:19:09 +0330

    class SendJson logic updated

--- rss_to_json/models.py
+++ rss_to_json/models.py
@@ -12,17 +12,65 @@ class SendJson(models.Model):
     page_size=0
 
     titles_list = []
+    descriptions_list = []
+    authors_list = []
+
     for item in data['items']:
         titles_list.append(item['title'])
-        page_size+=1
-    titles = titles_list
-
-    descriptions_list = []
-    for item in data['items']:
-        descriptions_list.append(item['description'])
-    descriptions = descriptions_list
-
-    authors_list = []
-    for item in data['items']:
         authors_list.append(item['author'])
-    authors = authors_list
+        descriptions_list.append(item['description'])
+        page_size+=1
+    
+    content_1 = {
+        "title" : titles_list[0],
+        "author" : authors_list[0],
+        "descriptions" : descriptions_list[0]
+    }
+    content_2 = {
+        "title" : titles_list[1],
+        "author" : authors_list[1],
+        "descriptions" : descriptions_list[1]
+    }
+    content_3 = {
+        "title" : titles_list[2],
+        "author" : authors_list[2],
+        "descriptions" : descriptions_list[2]
+    }
+    content_4 = {
+        "title" : titles_list[3],
+        "author" : authors_list[3],
+        "descriptions" : descriptions_list[3]
+    }
+    content_5 = {
+        "title" : titles_list[4],
+        "author" : authors_list[4],
+        "descriptions" : descriptions_list[4]
+    }
+    content_6 = {
+        "title" : titles_list[5],
+        "author" : authors_list[5],
+        "descriptions" : descriptions_list[5]
+    }
+    content_7 = {
+        "title" : titles_list[6],
+        "author" : authors_list[6],
+        "descriptions" : descriptions_list[6]
+    }
+    content_8 = {
+        "title" : titles_list[7],
+        "author" : authors_list[7],
+        "descriptions" : descriptions_list[7]
+    }
+    content_9 = {
+        "title" : titles_list[8],
+        "author" : authors_list[8],
+        "descriptions" : descriptions_list[8]
+    }
+    content_10 = {
+        "title" : titles_list[9],
+        "author" : authors_list[9],
+        "descriptions" : descriptions_list[9],
+    }
+        # titles = titles_list[page_size]
+        # authors = authors_list[page_size]
+        # descriptions = descriptions_list[page_size]
