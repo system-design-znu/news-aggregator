adeac362fd64147fbff2f271febd6c56f3211e01
Ali Safapour <thisisalinton@gmail.com>
2023-01-08 00:11:49 +0330

    Politeness added and containerization done

--- URL Fetch Unit/FetchUnit.py
+++ URL Fetch Unit/FetchUnit.py
@@ -1,14 +1,16 @@
 import asyncio
 import datetime
+import logging
 import aiohttp
 import pickle
+import uuid
 from aio_pika import (
     connect as rabbit_mq_connector,
     Message as rabbit_mq_message,
     DeliveryMode as rabbit_mq_delivery_mode,
     ExchangeType as rabbit_mq_exchange_type,
 )
-from aio_pika.exceptions import ChannelClosed as rabbit_mq_exchange_does_not_exists_exception
+from aio_pika.exceptions import ChannelClosed as rabbit_mq_does_not_exists_exception
 from aio_pika.abc import AbstractIncomingMessage
 from aiormq.exceptions import ChannelInvalidStateError
 from time import (
@@ -19,20 +21,22 @@ from signal import (
 )
 from threading import Event
 
-
-rabbit_mq_conn, rabbit_mq_conn_two, shutdown_future = [
+rabbit_mq_conn, shutdown_future, connection_is_ready = [
     None for i in range(3)]
 local_async_queue = asyncio.Queue()
+worker_unique_id = 'fetch_worker_' + str(uuid.uuid4()).replace('-', '')
+logging.basicConfig(format='%(asctime)s - %(levelname)s:  %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
 
 async def initializer():
-    global rabbit_mq_conn, rabbit_mq_conn_two
-    rabbit_mq_conn = await rabbit_mq_connector("amqp://guest:guest@localhost/")
+    global rabbit_mq_conn, connection_is_ready
+    connection_is_ready = asyncio.Future()
+    rabbit_mq_conn = await rabbit_mq_connector("amqp://guest:guest@host.docker.internal:18009/")
 
 
-async def on_message(message: AbstractIncomingMessage) -> None:
-    async with message.process():
-        await local_async_queue.put(message.body.decode('utf-8'))
+# async def on_message(message: AbstractIncomingMessage) -> None:
+#     async with message.process():
 
 
 async def url_job_receiver():
@@ -41,6 +45,8 @@ async def url_job_receiver():
     "URL Fetching Unit".
     """
     async with rabbit_mq_conn:
+        global connection_is_ready
+        connection_is_ready.set_result(0)
         rabbit_mq_channel = await rabbit_mq_conn.channel()
         await rabbit_mq_channel.set_qos(prefetch_count=1)
         try:
@@ -49,19 +55,25 @@ async def url_job_receiver():
             # channel (because it will be closed when an exception occurs) and
             # create a new exchange.
             await rabbit_mq_channel.declare_exchange('news_aggregator_direct', rabbit_mq_exchange_type.DIRECT, durable=True, passive=True)
-        except rabbit_mq_exchange_does_not_exists_exception:
+        except rabbit_mq_does_not_exists_exception:
             rabbit_mq_channel = await rabbit_mq_conn.channel()
             rabbit_mq_direct_exchange = await rabbit_mq_channel.declare_exchange('news_aggregator_direct', rabbit_mq_exchange_type.DIRECT, durable=True)
-            print('[Exchange Created] => news_aggregator_direct')
+            logging.info(
+                'A new rabbit mq exchange created: news_aggregator_direct')
         else:
             rabbit_mq_direct_exchange = await rabbit_mq_channel.declare_exchange('news_aggregator_direct', rabbit_mq_exchange_type.DIRECT, durable=True)
-        rabbit_mq_urls_queue = await rabbit_mq_channel.declare_queue('fetchable_url', durable=True)
-        await rabbit_mq_urls_queue.bind(rabbit_mq_direct_exchange, routing_key='fetchable_url')
-        await rabbit_mq_urls_queue.consume(on_message)
-        await asyncio.Future()
+        rabbit_mq_worker_queue = await rabbit_mq_channel.declare_queue(worker_unique_id, durable=True, exclusive=True)
+        await rabbit_mq_worker_queue.bind(rabbit_mq_direct_exchange, routing_key=worker_unique_id)
+        async with rabbit_mq_worker_queue.iterator() as iterator:
+            async for message in iterator:
+                async with message.process():
+                    logging.info(
+                        f'A new fetch job received: {message.body.decode("utf-8")}')
+                    await local_async_queue.put(message.body.decode('utf-8'))
 
 
 async def url_fetch_worker():
+    await connection_is_ready
     rabbit_mq_channel = await rabbit_mq_conn.channel()
     try:
         # Checking whether the exchange we want to use exists. If not, an
@@ -69,10 +81,11 @@ async def url_fetch_worker():
         # channel (because it will be closed when an exception occurs) and
         # create a new exchange.
         await rabbit_mq_channel.declare_exchange('news_aggregator_direct', rabbit_mq_exchange_type.DIRECT, durable=True, passive=True)
-    except rabbit_mq_exchange_does_not_exists_exception:
+    except rabbit_mq_does_not_exists_exception:
         rabbit_mq_channel = await rabbit_mq_conn.channel()
         rabbit_mq_direct_exchange = await rabbit_mq_channel.declare_exchange('news_aggregator_direct', rabbit_mq_exchange_type.DIRECT, durable=True)
-        print('[Exchange Created] => news_aggregator_direct')
+        logging.info(
+            'A new rabbit mq exchange created: news_aggregator_direct')
     else:
         rabbit_mq_direct_exchange = await rabbit_mq_channel.declare_exchange('news_aggregator_direct', rabbit_mq_exchange_type.DIRECT, durable=True)
     rabbit_mq_urls_queue = await rabbit_mq_channel.declare_queue('serialized_not_processed_xmls', durable=True)
@@ -81,7 +94,8 @@ async def url_fetch_worker():
         ready_url = await local_async_queue.get()
         async with aiohttp.ClientSession() as session:
             async with session.get(ready_url) as response:
-                received_response = await response.text()
+                received_response = (await response.text()).strip()
+
                 if '<?xml' in received_response[:10]:
                     try:
                         await rabbit_mq_direct_exchange.publish(
@@ -91,14 +105,20 @@ async def url_fetch_worker():
                             ),
                             routing_key=rabbit_mq_urls_queue.name
                         )
+                        logging.info(
+                            f'The fetch operation for the url "{ready_url}" has been done.')
                     except ChannelInvalidStateError as e:
-                        print(
-                            'Re opening the channel...')
+                        logging.critical(
+                            'Channel is closed. Reopening...')
                         rabbit_mq_channel = await rabbit_mq_conn.channel()
-
+                        logging.info('Channel opened again.')
+                else:
+                    logging.error(
+                        f'The provided url "{ready_url}" is not a valid rss. First 10 characters of fetched data: {received_response[:10]}')
+                    
+                    
 loop = asyncio.new_event_loop()
 loop.run_until_complete(initializer())
 loop.create_task(url_job_receiver())
-loop.run_until_complete(asyncio.sleep(2))
 loop.create_task(url_fetch_worker())
 loop.run_forever()
--- Url Generator Unit/GeneratorUnit.py
+++ Url Generator Unit/GeneratorUnit.py
@@ -1,11 +1,15 @@
 import asyncio
+import requests
+import aiohttp
+import logging
+import re
 from aio_pika import (
     connect as rabbit_mq_connector,
     Message as rabbit_mq_message,
     DeliveryMode as rabbit_mq_delivery_mode,
     ExchangeType as rabbit_mq_exchange_type,
 )
-from aio_pika.exceptions import ChannelClosed as rabbit_mq_exchange_does_not_exists_exception
+from aio_pika.exceptions import ChannelClosed as rabbit_mq_does_not_exists_exception
 from time import (
     time, sleep
 )
@@ -17,12 +21,82 @@ from threading import Event
 
 # Making asyncio.run work although there is an running event loop in the
 # current thread.
-import nest_asyncio
-nest_asyncio.apply()
+# import nest_asyncio
+# nest_asyncio.apply()
 
 rabbit_mq_conn = None
 shutdown_command_issued = False
 shutdown_event = asyncio.Event()
+
+available_workers = dict()
+logging.basicConfig(format='%(asctime)s - %(levelname)s:  %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
+def get_workers_list(user='guest', password='guest', host='host.docker.internal', port=18010, virtual_host=None):
+    url = 'http://%s:%s/api/queues/%s' % (host, port, virtual_host or '')
+    response = requests.get(url, auth=(user, password))
+    queues = [q['name'] for q in response.json()]
+    return queues
+
+async def get_workers_list_async(user='guest', password='guest', host='host.docker.internal', port=18010, virtual_host=None):
+    async with aiohttp.ClientSession() as session:
+        url = 'http://%s:%s/api/queues/%s' % (host, port, virtual_host or '')
+        async with session.get(url, auth=aiohttp.BasicAuth(user, password)) as response:
+            workers = [worker['name'] for worker in await response.json() if 'fetch_worker' in worker['name']]
+            return workers
+
+
+async def add_fetch_worker(user='guest', password='guest', host='host.docker.internal', port=18010, virtual_host=None):
+    global available_workers
+    url = 'http://%s:%s/api/queues/%s' % (host, port, virtual_host or '')
+    while True:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, auth=aiohttp.BasicAuth(user, password)) as response:
+                result = await response.json()
+                workers = [worker['name'] for worker in result]
+                for worker in workers:
+                    if 'fetch_worker' in worker:
+                        if worker not in available_workers.keys():
+                            logging.info(f'A new fetch worker joined to the system: "{worker}"')
+                            available_workers.update(
+                                {
+                                    worker: dict()
+                                }
+                            )
+        await asyncio.sleep(3)
+
+
+async def check_worker_is_alive():
+    rabbit_mq_channel = await rabbit_mq_conn.channel()
+    try:
+        await rabbit_mq_channel.declare_queue('serialized_not_processed_xmls', durable=True)
+    except rabbit_mq_does_not_exists_exception:
+        return 
+    
+
+async def choose_worker(url):
+    global available_workers
+    pattern = r'^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)'
+    domain = re.search(
+        pattern, url).group(1)
+    fetch_intervals_per_domain = 5  # 10 Minutes
+    # available_workers = { worker: { 'domain': 'last_fetch_time' } }
+    temp_available_workers = available_workers.copy()
+    for worker, worker_info in available_workers.items():
+        if not worker in await get_workers_list_async():
+            del temp_available_workers[worker]
+            logging.error(f'The worker "{worker}" has been disconnected.')
+            continue
+        if domain in available_workers[worker].keys():
+            if time() - available_workers[worker][domain] > fetch_intervals_per_domain:
+                if worker in await get_workers_list_async():
+                    return worker
+            else:
+                continue
+        else:
+            return worker
+    available_workers = temp_available_workers
+    return None
+
 
 # This kind of representation of the URLs and their "fetch-count per 5 minutes"
 # is temporary. These data will be extracted from the database in future
@@ -31,22 +105,27 @@ rss_container = [
     {
         'url': 'https://www.isna.ir/rss/tp/149',
         'fetch_counts_per_five_minutes': 300,
+        'priority': 1
     },
     {
         'url': 'https://fararu.com/fa/rss',
-        'fetch_counts_per_five_minutes': 150
+        'fetch_counts_per_five_minutes': 150,
+        'priority': 1
     },
     {
         'url': 'https://www.shahrekhabar.com/rssfeeds',
-        'fetch_counts_per_five_minutes': 10
+        'fetch_counts_per_five_minutes': 10,
+        'priority': 2
     },
     {
         'url': 'https://www.khabarvarzeshi.com/rss-help',
-        'fetch_counts_per_five_minutes': 1
+        'fetch_counts_per_five_minutes': 1,
+        'priority': 2
     },
     {
         'url': 'https://www.khabaronline.ir/rss-help',
-        'fetch_counts_per_five_minutes': 3
+        'fetch_counts_per_five_minutes': 3,
+        'priority': 3
     },
 ]
 
@@ -70,6 +149,7 @@ for rss_info in rss_container:
         }
     )
 
+rss_container.sort(key=lambda x: x['priority'], reverse=True)
 
 async def gracefully_async_exit_handler():
     await shutdown_event.wait()
@@ -92,13 +172,13 @@ def gracefully_sync_exit_handler(sig, frame):
     quit()
 
 
-# Handling the interrupt signal.
-signal(SIGINT, gracefully_sync_exit_handler)
+# # Handling the interrupt signal.
+# signal(SIGINT, gracefully_sync_exit_handler)
 
 
 async def initializer():
     global rabbit_mq_conn
-    rabbit_mq_conn = await rabbit_mq_connector("amqp://guest:guest@localhost/")
+    rabbit_mq_conn = await rabbit_mq_connector("amqp://guest:guest@host.docker.internal:18009/")
 
 
 async def rss_container_updater():
@@ -110,7 +190,7 @@ async def generator():
     and sends the processable URLs to a RabbitMQ exchange to be received by
     "URL Fetching Unit".
     """
-    global shutdown_command_issued, shutdown_event
+    global shutdown_command_issued, shutdown_event, available_workers
     async with rabbit_mq_conn:
         rabbit_mq_channel = await rabbit_mq_conn.channel()
         try:
@@ -119,39 +199,46 @@ async def generator():
             # channel (because it will be closed when an exception occurs) and
             # create a new exchange.
             await rabbit_mq_channel.declare_exchange('news_aggregator_direct', rabbit_mq_exchange_type.DIRECT, durable=True, passive=True)
-        except rabbit_mq_exchange_does_not_exists_exception:
+        except rabbit_mq_does_not_exists_exception:
             rabbit_mq_channel = await rabbit_mq_conn.channel()
             rabbit_mq_direct_exchange = await rabbit_mq_channel.declare_exchange('news_aggregator_direct', rabbit_mq_exchange_type.DIRECT, durable=True)
-            print('[Exchange Created] => news_aggregator_direct')
+            logging.info('A new rabbit mq exchange created: news_aggregator_direct')
         else:
             rabbit_mq_direct_exchange = await rabbit_mq_channel.declare_exchange('news_aggregator_direct', rabbit_mq_exchange_type.DIRECT, durable=True)
-        rabbit_mq_urls_queue = await rabbit_mq_channel.declare_queue('fetchable_url', durable=True)
-        await rabbit_mq_urls_queue.bind(rabbit_mq_direct_exchange, routing_key='fetchable_url')
+        pattern = r'^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)'
         while True:
             if not shutdown_command_issued:
                 shutdown_command_issued = False
                 for index, rss_info in enumerate(rss_container):
-                    # print('this is the rss info0', rss_info)
                     if time() - rss_info['last_process_time'] > rss_info['next_fetch_happens_in']:
                         rss_info['last_process_time'] = time()
-                        print(f'[{datetime.datetime.fromtimestamp(time()).strftime("%Y-%m-%d %H:%M:%S")}] \
-> Putting the url {rss_info["url"]} in the RabbitMQ\'s exchange.')
-                        await rabbit_mq_direct_exchange.publish(
-                            rabbit_mq_message(
-                                bytes(rss_info['url'], encoding='utf-8'),
-                                delivery_mode=rabbit_mq_delivery_mode.PERSISTENT
-                            ),
-                            routing_key=rabbit_mq_urls_queue.name
-                        )
-                        print('sent')
+                        if chosen_worker:=await choose_worker(rss_info['url']):
+                            await rabbit_mq_direct_exchange.publish(
+                                rabbit_mq_message(
+                                    bytes(rss_info['url'], encoding='utf-8'),
+                                    delivery_mode=rabbit_mq_delivery_mode.PERSISTENT
+                                ),
+                                routing_key=chosen_worker
+                            )
+                            temp_dict = available_workers[chosen_worker].copy()
+                            temp_dict.update(
+                                {
+                                    re.search(pattern, rss_info['url']).group(1): time()
+                                }
+                            )
+                            available_workers[chosen_worker] = temp_dict
+                            logging.info(f'The url "{rss_info["url"]}" putted and will be fetched by the worker "{chosen_worker}" soon.')
+                        else:
+                            logging.critical(f'There is no ready worker to fetch the url "{rss_info["url"]}".')
                 await asyncio.sleep(0.5)
             else:
                 break
-    print('The connection with the RabbitMQ node closed.')
+    logging.info('The connection with the RabbitMQ node closed.')
     shutdown_event.set()
 
 
 loop = asyncio.new_event_loop()
 loop.run_until_complete(initializer())
 loop.create_task(generator())
+loop.create_task(add_fetch_worker())
 loop.run_forever()
