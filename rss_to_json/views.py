79fa47a5d224241a79c2d8e5abb253c72b0e7914
0-Behnam-0 <Mahdi.mohamadiha.znu@gmail.com>
2023-02-22 15:25:03 +0330

    permission layer updated

--- rss_to_json/views.py
+++ rss_to_json/views.py
@@ -5,6 +5,6 @@ from rest_framework import generics, permissions
 # Create your views here.
 
 class SendJsonList(generics.CreateAPIView):
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (permissions.AllowAny,)
     queryset = SendJson.objects.all()
     serializer_class = SendJsonSerializer
