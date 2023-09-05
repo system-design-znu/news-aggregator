d55b752f73d63ed803d139ca39eece96566a06e4
0-Behnam-0 <Mahdi.mohamadiha.znu@gmail.com>
2023-09-05 12:06:20 +0330

    null and blank is acceptable and publish date type is string now

--- rss_to_json/models.py
+++ rss_to_json/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from .data import News
 
 # Create your models here.
 
 class Archive(models.Model):
-    title = models.CharField(max_length=126)
-    author = models.CharField(max_length=126)
-    publish_date = models.DateTimeField("Publish date")
+    title = models.CharField(max_length=126, null=True, blank=True)
+    author = models.CharField(max_length=126, null=True, blank=True)
+    publish_date = models.CharField("Publish date (as string)", max_length=126, null=True, blank=True)
