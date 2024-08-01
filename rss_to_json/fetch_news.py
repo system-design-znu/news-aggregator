72cca68e9672f62166bdcb8036ba4ad8c870a5a4
MahdiMohammadiha <mahdi.mohamadiha.znu@gmail.com>
2024-08-01 19:11:04 +0330

    new news api

--- rss_to_json/fetch_news.py
+++ rss_to_json/fetch_news.py
@@ -1,30 +1,34 @@
-from json import loads
 import urllib.request as ulrr
+import xml.etree.ElementTree as ET
 
 class News:
 
     @staticmethod
     def irna_news():
-        url = "https://api.rss2json.com/v1/api.json?rss_url=https://www.irna.ir/rss"
+        url = "https://www.irna.ir/rss"
         response = ulrr.urlopen(url).read().decode('utf8')
-        disarray_data = loads(response)
-        raw_information = disarray_data['items']
+
+        # Parse XML data
+        root = ET.fromstring(response)
 
         titles_list = []
         authors_list = []
         dates_list = []
-        for ele in range(len(raw_information)):
-            titles_list.append((raw_information[ele])['title'])
-            authors_list.append((raw_information[ele])['author'])
-            dates_list.append((raw_information[ele])['pubDate'])
+        for item in root.findall('.//item'):
+            title = item.find('title').text if item.find('title') is not None else 'No title'
+            author = item.find('author').text if item.find('author') is not None else 'No author'
+            pub_date = item.find('pubDate').text if item.find('pubDate') is not None else 'No date'
 
-        cnt = 0
+            titles_list.append(title)
+            authors_list.append(author)
+            dates_list.append(pub_date)
+
         data = []
-        for cnt in range(10):
+        for cnt in range(min(10, len(titles_list))):
             data_dict = {
-                'title' : titles_list[cnt],
-                'author' : authors_list[cnt],
-                'publish date' : dates_list[cnt],
+                'title': titles_list[cnt],
+                'author': authors_list[cnt],
+                'publish date': dates_list[cnt],
             }
             data.append(data_dict)
 
@@ -33,6 +37,10 @@ class News:
 
 if __name__ == '__main__':
     def fetch_and_print_news():
-        print(News.irna_news())
+        news = News.irna_news()
+        for item in news:
+            print(f"Title: {item['title']}")
+            print(f"Author: {item['author']}")
+            print(f"Publish Date: {item['publish date']}\n")
 
     fetch_and_print_news()
